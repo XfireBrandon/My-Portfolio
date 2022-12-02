fa3e9f61741862fa6984008a3c905319565c9185
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-12-02 10:30:15 -0700

    scroll click working

--- client/my-portfolio/src/App.js
+++ client/my-portfolio/src/App.js
@@ -1,25 +1,50 @@
-import Header from "./components/HeaderComponets/Header";
+// import Header from "./components/HeaderComponets/Header";
 import About from "./components/Sections/About";
 import NavBar from "./components/NavBarComponents/NavBar"
 import Projects from "./components/Sections/Projects";
 import Contact from "./components/Sections/Contact";
-import ParticlesBg from 'particles-bg'
+// import ParticlesBg from 'particles-bg'
 
 import { useRef } from "react";
 import './CSS/styles.css'
 
 function App() {
 
+  const contact = useRef(null);
+  const projects = useRef(null);
+  const about = useRef(null);
 
+
+  const contactClick = () => {
+    contact.current?.scrollIntoView({behavior: 'smooth'});
+  };
+
+  const projectClick = () => {
+    projects.current?.scrollIntoView({behavior: 'smooth'});
+  };
+
+  const aboutClick = () => {
+    about.current?.scrollIntoView({behavior: 'smooth'});
+  };
+
+  
+  
   return (
     <>
     <div className="HeaderDiv">
-      <Header />
-      <NavBar />
+      <h2 onClick={contactClick}>Contact</h2>
+      <h2 onClick={projectClick}>Projects</h2>
+      <h2 onClick={aboutClick}>About</h2>
+      <div ref={about}>
       <About />
+       </div>
+      <div ref={projects}>
       <Projects />
+      </div>
+      <div ref={contact}>
       <Contact />
-    <ParticlesBg color='#FFBF00' type="cobweb" bg={true} num={250}/>
+      </div>
+    {/* <ParticlesBg color='#FFBF00' type="cobweb" bg={true} num={250}/> */}
     </div>
     </>
   );
--- client/my-portfolio/src/components/NavBarComponents/NavBar.js
+++ client/my-portfolio/src/components/NavBarComponents/NavBar.js
@@ -5,6 +5,7 @@ const NavBar = () => {
         <nav className="nav" id="navbar">
             <div className="nav-content">
                 <ul className="nav-items">
+                    <h1>My Portfolio</h1>
                     <li className="nav-item">About Me</li>
                     <li className="nav-item">Projects</li>
                     <li className="nav-item">Contact Me</li>
