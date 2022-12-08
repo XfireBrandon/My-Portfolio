f2dc4a203395b7617b69c53fe2db367cdbf624bc
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-12-07 18:50:53 -0700

    finished formating

--- client/my-portfolio/src/components/Sections/About.js
+++ client/my-portfolio/src/components/Sections/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 but thats neither here nor there. Early in 2022 I decided to take a big leap into where I want to be in the future and decided to transition out of the military to become a Software Engineer.
                  In July 2022, I started my attendance at Galvanzie, a Code Boot Camp where I worked on several projects, some you'll see below, this journey has so much fun so far and I'm so excited to see where I will go.</p>
             <p>Outside of work I like to disconnect from the computer for a bit, some of my hobbies include: Fishing when the weather is nice, Gaming, relaxing with my family, and just enjoying the outdoors.
-                 I would consider myself a family man, aswell as a pet loving one as well, my family consist of my beautiful wife, 2 beautiful daughters, 3 cats, 2 dogs.</p>
+                 I would consider myself a family man, aswell as a pet loving one as well, my family consist of my beautiful wife, two beautiful daughters, three cats, two dogs.</p>
 
         </div>
     )
